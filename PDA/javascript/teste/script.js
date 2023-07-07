49e2df88628cb0f363e690af27213a4658e68eb2
Gab <you@example.com>
2023-07-07 01:07:15 -0300

    aprendi Javascript

---
+++ PDA/javascript/teste/script.js
@@ -0,0 +1,19 @@
+let precoMax = 1900;
+let polegadaMin = 32;
+let polegada = 36;
+let preco = 2000;
+
+if(preco <= precoMax && polegada >= polegadaMin){
+    console.log('Vai na fé');
+}
+else if(polegada  < polegadaMin && preco <precoMax){
+    consolelog('TV ruim');
+}
+else if(polegada < polegadaMin){
+    console.log('TV pequena')
+}
+else{
+    console.log('espere uma promocao')
+}
+
+    
